ba00de79e9acd7d4e714d8a94a6f7097bdd7a0cb
Xiaoying-Huang <hxybecomexs@gmail.com>
2022-10-17 20:33:52 +0100

    up to task 55

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -29,8 +29,27 @@ class App extends React.Component{
         album: "Al2",
         id: "id2"
         }]
-    }
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
+
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } 
+
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track){
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState({playlistTracks: tracks});
+  }
+
   render() {
     return (
       <div>
@@ -38,8 +57,11 @@ class App extends React.Component{
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults = {this.state.searchResults} />
-            <Playlist playlistName = {this.state.playlistName} playlistTracks = {this.state.playlistTracks} />
+            <SearchResults searchResults = {this.state.searchResults}
+            onAdd = {this.addTrack} />
+            <Playlist playlistName = {this.state.playlistName}
+            playlistTracks = {this.state.playlistTracks}
+            onRemove = {this.removeTrack} />
           </div>
         </div>
       </div>
